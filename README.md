5aaa601b3b9ab57003bfb0205ff12fc1dc088554
rememberlenny <leonard_bogdonoff@condenast.com>
2015-07-14 13:41:06 -0400

    Mock

---
+++ README.md
@@ -0,0 +1,3 @@
+# Chrome Extension Template
+This is a template Chrome extension that just has placeholders for a lot of the basic
+components to get you started.
---
+++ assets/js/routes.js
@@ -0,0 +1,1 @@
+var foundationRoutes = [{"name":"settings","url":"/settings","animationIn":"slideFromLeft","animationOut":"slideFromLeft","path":"templates/settings.html"},{"name":"home","url":"/","path":"templates/home.html"}]; 
